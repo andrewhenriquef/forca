9ab819139561d85fd11f52cb54ce0a94550f6c9c
Andrew Henrique <andrewhenriquef@gmail.com>
2016-12-04 20:18:22 -0200

    Bug-fix

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -2,15 +2,53 @@
   (:gen-class))
 
 
-(def numeros [1 2 3 4 5])
-(def carros [50000.0 60000.0])
-
-;(remove (fn [x] (= (rem x 2) 1)) numeros)
-
 (def total-de-vidas 6)
 
 (defn perdeu [](print " você perdeu\n --->"))
+
 (defn ganhou [](print " você ganhou\n --->"))
+
+(defn letras-faltantes [palavra acertos]
+  (remove (fn [letra] (contains? acertos (str letra))) palavra))
+
+;verifica se a lista esta vazia
+(defn acertou-a-palavra-toda? [palavra acertos]
+  (empty? (letras-faltantes palavra acertos)))
+
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra] (.contains palavra chute))
+
+(defn avalia-chute [chute vidas palavra acertos]
+    (if (acertou? chute palavra)
+        (jogo vidas palavra (conj acertos chute))
+        (jogo (dec vidas) palavra acertos)
+    )
+)
+
+(defn jogo [vidas palavra acertos]
+    (if (=vidas 0)
+        (perdeu)
+        (if (acertou-a-palavra-toda? palavra acertos)
+            (ganhou)
+            (avalia-chute (le-letra!) vidas palavra acertos))))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+
+
+
+
+
+ 
+;(def numeros [1 2 3 4 5])
+;(def carros [50000.0 60000.0])
+
+
+
 
 ;(defn breakingLines [](print "\n"))
 
@@ -21,24 +59,8 @@
 
 ;(map (fn [letra](contains? (str letra)))palavra)
 
-(map (fn [letra] (contains? (str letra)))palavra)
-
-(defn letras-faltantes [palavra acertos]
-  (remove (fn [letra] (contains? acertos (str letra))) palavra))
-
-;verifica se a lista esta vazia
-(defn acertou-a-palavra-toda? [palavra acertos]
-  (empty? (letras-faltantes palavra acertos)))
-
-(defn jogo [vidas palavra acertos]
-  (if (= vidas 0)
-    (perdeu)
-    (if (acertou-a-palavra-toda? palavra acertos)
-      (ganhou)
-      (print "Chuta, amigo!")
-    )
-  )
-)
+;(map (fn [letra] (contains? (str letra)))palavra)
+;(remove (fn [x] (= (rem x 2) 1)) numeros)
 
 ;filter list number < 2 or > 4 ;;; 1, 5
 ;(filter (fn [x](or (< x 2)(> x 4)))numeros)
@@ -63,8 +85,3 @@
 ;   [x]
 ;   (* x
 ;     (Math/pow 2 x)))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
