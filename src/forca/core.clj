916d221fffc884705b75ecbe293e01559693c146
Andrew Henrique <andrewhenriquef@gmail.com>
2016-12-06 11:00:43 -0200

    Finished exercises of recursive functions

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -1,6 +1,7 @@
 (ns forca.core
   (:gen-class))
 
+
 (def total-de-vidas 6)
 
 (defn perdeu [] (print "Você perdeu!"))
@@ -20,19 +21,34 @@
 (declare avalia-chute)
 
 (defn jogo [vidas palavra acertos]
-    (if (= vidas 0)
-        (perdeu)
-        (if (acertou-a-palavra-toda? palavra acertos)
-            (ganhou)
-            (avalia-chute (le-letra!) vidas palavra acertos))))
-
-(defn avalia-chute [chute vidas palavra acertos]
-  (if (acertou? chute palavra)
-   (jogo vidas palavra (conj acertos chute))
-   (jogo (dec vidas) palavra acertos)))
-
+    (cond
+      (= vidas 0)(perdeu)
+      (acertou-a-palavra-toda? palavra acertos)(ganhou)
+      :else
+      (let [chute (le-letra!)]
+        (if (acertou? chute palavra)
+         (do
+           (println "Acertou a letra !")
+           (recur vidas palavra (conj acertos chute)))
+         (do
+            (println "Errou a letra !")
+            (recur (dec vidas) palavra acertos))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
+
+;recursive fibonacci
+; (defn fib[x]
+;   (loop [a 1 b 1 numero 2]
+;     (if (= numero x) b
+;      (recur b (+ a b)(inc numero)))))
+
+;Recursive sum
+; (defn soma[n]
+;    (loop [contador 1 soma 0]
+;      (if (> contador n) soma
+;       (recur (inc contador) (+ soma contador)))))
